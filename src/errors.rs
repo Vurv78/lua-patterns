ab2abc1d8735a7d39e1d34acef33b2a66b7a4dff
Vurv <56230599+Vurv78@users.noreply.github.com>
2022-04-10 00:21:02 -0700

    lupat

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,13 @@
-use std::fmt;
 use std::error::Error;
+use std::fmt;
 
 /// Error type returned by _try methods
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct PatternError(pub String);
 
 impl fmt::Display for PatternError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f,"{}",self.0)
+		write!(f, "{}", self.0)
 	}
 }
 
@@ -16,4 +16,3 @@ impl Error for PatternError {
 		&self.0
 	}
 }
-
